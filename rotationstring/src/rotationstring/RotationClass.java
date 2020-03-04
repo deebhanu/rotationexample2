61a6b6755dff25c2cbede9de5f32e9c607ba0acf
DeeBhanu <deepakbhanushali.2016@gmail.com>
2020-03-04 20:18:35 +0530

    first commit upload all files

---
+++ rotationstring/src/rotationstring/RotationClass.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package rotationstring;
+
+/**
+ * @author Owner
+ *
+ */
+public class RotationClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="nilam";
+String temp=a;
+String b=a.substring(2, 5);
+String c=a.substring(0, 2);
+System.out.println(b);
+System.out.println(c);
+String d=b.concat(c);
+System.out.println(d);
+//String e=a.substring(2, 5).concat(a.substring(0, 2));
+//System.out.println(e);
+	}
+
+}
